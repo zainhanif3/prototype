532bd725d658ac413009e8ce044f151d701ea470
zainhanif3 <juttgee4011@gmail.com>
2024-02-07 17:53:20 +0500

    commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const path = require(path)
+const path = require("path");
 const app = express();
 const port = 3000;
+const hbs = require ("hbs")
 
 mongoose.connect('mongodb://localhost:27017/prototype', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use('views', express.static('views'))
+
 
 const User = mongoose.model('User', {
     name: String,
@@ -24,12 +28,24 @@ const User = mongoose.model('User', {
     }
 });
 
-
+console.group(__dirname, "views" );
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.set('view engine', 'html');
-
+app.set('view engine', 'hbs');
+//router
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+app.get('/signup', (req, res)=>{
+    res.render('signup')
+})
+app.get('/signin', (req, res)=>{
+    res.render('signin')
+})
+app.get('/password', (req, res)=>{
+    res.render('password')
+})
 // Render the user registration page
-app.get('/views/signup', (req, res) => {
+app.get('/signup', (req, res) => {
     res.render('signup');
 });
 
@@ -54,13 +70,49 @@ app.post('/register', async (req, res) => {
         });
 
         await newUser.save();
-        res.redirect('/portal'); // Redirect to portal or any other page
+        res.redirect('/signin'); // Redirect to portal or any other page
     } catch (error) {
         console.error('Error saving user:', error);
         res.status(500).send('Internal Server Error');
     }
 });
 
+//sign in check
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user by email
+    const user = await User.findOne({ email });
+    
+  
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email ' });
+    }
+
+    // Compare the provided password with the hashed password stored in the database
+    
+
+    if (user.password === password) {
+      res.status(200).redirect('/portal');
+      
+    }else{
+      res.send("password not match");
+    }
+    
+   
+
+    // Redirect to the portal page after successful sign-in
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
